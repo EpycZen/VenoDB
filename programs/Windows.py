8cbbc83f40e1e373610bc6bb109918345d65ec2a
Sandeep Kumar <56790764+EpycZen@users.noreply.github.com>
2019-12-17 11:39:16 +0530

    Update Windows.py

--- programs/Windows.py
+++ programs/Windows.py
@@ -1,5 +1,5 @@
 ###################################################################################################
-# Windows.py is a module for executing windows commands more easily
+# Windows.py is a module for executing windows commands for VenoDB
 
 # Author : EpycZen ~ GitHub | StackOverFlow | Twitter | Instagram
 # With fixes/improvements from :
@@ -7,6 +7,8 @@
 ###################################################################################################
 import os
 import subprocess
+
+installation_dir = ""
 
 
 def pause():  # just a normal pause. displays "Press any key to continue . . ."
@@ -25,6 +27,24 @@ def color(str1):  # for selecting the color on the command prompt screen
     os.system("color " + str1)
 
 
+def installation_directory():  # to set the installation directory from a file
+    try:
+        f = open("INSTALLATION_DIRECTORY.txt", "r")
+        lines = f.readlines()
+        for line in lines:
+            if "installation_dir" in line:
+                startq = line.find("`") + 1
+
+                endq = line.find("\'")
+
+                global installation_dir
+                installation_dir += line[startq:endq]
+
+    except FileNotFoundError:
+        print("[Error: IDNF] INSTALLATION_DIRECTORY.txt not found. Refer documentation for more help")
+        return
+
+
 def file_create(name, dbname, headings):  # creates an empty file in the current working directory
     cddir("data\\" + dbname)
     try:
@@ -37,6 +57,13 @@ def file_create(name, dbname, headings):  # creates an empty file in the current
         f1.write(headings)
 
 
+def folder_check(database_name):
+    if database_name in list_directories():
+        return False
+    else:
+        return True
+
+
 def folder_create(name):  # creates a folder in the current working directory
     cddir("data")
     try:
@@ -47,17 +74,12 @@ def folder_create(name):  # creates a folder in the current working directory
 
 
 def cddir(name):  # changes the directory
-    user_profile = subprocess.check_output("echo %USERPROFILE%", shell=True)  # to get the current username
-    user_profile = user_profile.decode('utf-8')  # to decode it to str
-    user_profile = user_profile.replace("\r\n", "")
+    os.chdir(installation_dir)  # to switch to the installation directory
 
-    installation_directory = user_profile + "\\PycharmProjects\\CSQL"  # string containing the installation directory
-
-    try:
-        os.chdir(installation_directory)
+    try:  # try switching to the given directory
         os.chdir(name)
 
-    except FileNotFoundError:
+    except FileNotFoundError:  # if the directory is not found
         print("No such folder/file exists")
         return -1
 
@@ -67,13 +89,12 @@ def list_files(database_name):  # returns the files in a directory as a string
         return -1
 
     else:
-        cddir("data\\" + database_name)
         result = subprocess.check_output("dir /b /a-d", shell=True)  # returns all the files in "data" folder
-        return result
+        return str(result.decode('utf-8'))
 
 
 def list_directories():  # returns the directories as a string
     cddir("data")
     result = subprocess.check_output("dir /b/ ad", shell=True)  # returns all the directories inside a parent directory
 
-    return result
+    return str(result.decode('utf-8'))
